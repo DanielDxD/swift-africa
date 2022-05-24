97b1bf528768f5aa71e9460b55c6cfc0445cc64b
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-24 11:51:28 -0300

    Creating cover and decoding json data

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Daniel Lopes on 24/05/22.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
